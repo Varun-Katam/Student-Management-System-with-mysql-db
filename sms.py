841f23d260231457435c2452e699a3b5e61a8853
Varun-Katam <varunabhi3327@gmail.com>
2026-01-12 20:49:48 +0530

    Student management system with sql database

---
+++ sms.py
@@ -0,0 +1,69 @@
+import mysql.connector
+def connect_db():
+    connect = mysql.connector.connect(
+        host = "127.0.0.1",
+        port = 3306,
+        user = "root",
+        password = "Varun@3008",
+        database = "python"
+    )
+    return connect
+def add_student():
+    name = input("Enter a student name: ")
+    roll_no = int(input("Enter student roll number: "))
+    marks = int(input("Enter student marks: "))
+    conn = connect_db()
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO info (id, name, marks) VALUES (%s, %s, %s)", (roll_no, name, marks))
+    conn.commit()
+    conn.close()
+    print("Student added successfully")
+def view_student():
+    conn = connect_db()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM info")
+    rows = cursor.fetchall()
+    conn.close()
+    for i in rows:
+        print(i)
+def update_student():
+    sid = int(input("Enter student roll number: "))
+    marks = int(input("Enter marks to update: "))
+    conn = connect_db()
+    cursor = conn.cursor()
+    cursor.execute("UPDATE info SET marks = %s WHERE id = %s", (marks, sid))
+    conn.commit()
+    conn.close()
+    print("Student updated successfully")
+def delete_student():
+    sid = int(input("Enter student roll number: "))
+    conn = connect_db()
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM info WHERE id = %s", (sid,))
+    conn.commit()
+    conn.close()
+    print("Student deleted successfully")
+
+while True:
+    print("-"*15 + "MAIN MENU" + "-"*15)
+    print("1. Add Student\n2. View Student\n3. Update Student\n4. Delete Student\n5.Exit")
+    choice = int(input("Enter the choice from the above menu: "))
+    match choice:
+        case 1:
+            add_student()
+        case 2:
+            view_student()
+        case 3:
+            update_student()
+        case 4:
+            delete_student()
+        case 5:
+            print("_"*15 + "THANK YOU" + "_"*15)
+            break
+        case _:
+            print("Please choose a valid choice")
+
+
+
+
+
